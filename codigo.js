d9624e715c94d9b929383f14a34d7a45747ffdcb
Jgreyr <jgrey@fiuna.edu.py>
2024-08-21 21:13:23 -0400

    Version final

--- codigo.js
+++ codigo.js
@@ -5,7 +5,6 @@ function showSection(sectionId) {
         sections[i].style.display = 'none';
     }
 
-
     // Muestra la sección seleccionada
     var section = document.getElementById(sectionId);
     if (section) {
@@ -30,60 +29,3 @@ function moveSlide(direction) {
 
     container.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
 }
-
-function toggleMenu() {
-    var navItems = document.querySelector('.nav-items');
-    var menuBtn = document.getElementById('menu-btn');
-
-    navItems.classList.toggle('show');
-
-    if (navItems.classList.contains('show')) {
-        console.clear();
-        menuBtn.classList.add('close');
-        menuBtn.innerText = '✖'; // Cambia a "X"
-    } else {
-        menuBtn.innerHTML = '&#9776;'; // Cambia al icono de menú
-        menuBtn.classList.remove('close');
-    }
-}
-
-function isTouchDevice() {
-    return 'ontouchstart' in window || navigator.maxTouchPoints;
-}
-
-if (isTouchDevice()) {
-    document.querySelectorAll('.dropbtn, .submenu').forEach(function(item) {
-        item.addEventListener('click', function(e) {
-            e.preventDefault(); // Evitar el salto de anclaje
-            // if (item.classList.contains('submenu-toggle')) {
-                toggleSubmenu(this);
-            // } else {
-            //     toggleDropdown(this);
-            // }
-        });
-    });
-}
-
-function toggleSubmenu(element) {
-    var submenuContent = element.nextElementSibling;
-    submenuContent.classList.toggle('show');
-}
-document.querySelector('.menu').addEventListener('touchstart', function(e) {
-    // Tu código aquí
-});
-function isMobile() {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-}
-
-function redirectToMobile() {
-    if (isMobile()) {
-        alert("Ismobile")
-        window.location.href = "www.google.com";  // Redirigir al HTML para móviles
-    } else {
-        alert("IsNOTmobile")
-    }
-
-}
-
-// Llama a la función cuando la página se carga
-redirectToMobile();
