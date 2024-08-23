abaa265cab8156e52f26fd0c4a7e6462b935826c
Jgreyr <jgrey@fiuna.edu.py>
2024-08-23 15:50:39 -0400

    Publicación version 1

--- codigo.js
+++ codigo.js
@@ -11,9 +11,10 @@ function showSection(sectionId) {
         section.style.display = 'block';
     }
 }
+
 let currentIndex = 0;
 
-function moveSlide(direction) {
+function moveSlidePreguntas(direction) {
     const container = document.querySelector('.preguntas');
     const totalSlides = document.querySelectorAll('.pregunta').length;
     const slideWidth = document.querySelector('.pregunta').offsetWidth;
@@ -28,4 +29,32 @@ function moveSlide(direction) {
     }
 
     container.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+}
+
+// Crear un objeto para almacenar los índices de cada slider
+let slideIndices = {};
+
+function desplazarSlideInformacion(direction, sliderId) {
+    if (!slideIndices[sliderId]) {
+        slideIndices[sliderId] = 0; // Inicializar el índice si no existe
+    }
+
+    const contenedor = document.querySelector(`#${sliderId} .infoPreguntas`);
+    const diapositivas = document.querySelectorAll(`#${sliderId} .masInfo`);
+    const anchoContenedor = contenedor.parentElement.offsetWidth;
+    const anchoDiapositiva = diapositivas[0].offsetWidth;
+    const margenDiapositiva = parseFloat(getComputedStyle(diapositivas[0]).marginRight);
+
+    const anchoTotal = diapositivas.length * anchoDiapositiva + (diapositivas.length - 1) * margenDiapositiva;
+    const indiceMaximoDiapositiva = Math.ceil(anchoTotal / anchoContenedor) - 1;
+
+    slideIndices[sliderId] += direction;
+
+    if (slideIndices[sliderId] > indiceMaximoDiapositiva) {
+        slideIndices[sliderId] = indiceMaximoDiapositiva;
+    } else if (slideIndices[sliderId] < 0) {
+        slideIndices[sliderId] = 0;
+    }
+
+    contenedor.style.transform = `translateX(-${slideIndices[sliderId] * anchoContenedor}px)`;
 }
